119dd2c4edcc6f607db3c704f8737bf591a9bf4d
andrewmontes87 <andrewmontes87@gmail.com>
2017-11-26 12:16:51 -0800

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'node-ec2-test',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-54-209-121-121.compute-1.amazonaws.com',
+      key: '~/.ssh/node-ec2-test.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/andrewmontes87/node-ec2-test.git',
+      path: '/home/ec2-user/node-ec2-test',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
